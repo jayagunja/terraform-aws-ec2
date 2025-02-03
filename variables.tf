bd665299ee529c5408d8b5230645b12b3b7265c3
jayanthi gunja <jayanthi.tech94@gmail.com>
2025-02-03 21:36:53 +0530

    terraform module

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-09c813fb71547fc4f" 
+}
+
+variable "sg_id" {
+  
+}
+
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro"
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "Valid values for var: instance type are (t3.micro, t3.small, t3.medium)"
+  } 
+
+}
+
+variable "tags" {
+ default = {}
+}
